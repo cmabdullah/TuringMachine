7e3e5e8cc9bdbfdb50179960858679e7befad004
cmabdullah <cmabdullah21@gmail.com>
2017-11-22 20:19:05 +0600

    move turnig machine project from gitlab SolvedProblems directory and commit SHA is => 110d30d32063a666fe421d3683dbaa7992b65ea8

---
+++ TuringMachine.c
@@ -0,0 +1,97 @@
+/*
+ ============================================================================
+ Name        : TuringMachine.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include<stdio.h>
+char inp[20];
+int state = 0, head = 0;
+int main(){
+	scanf("%s",&inp);
+    while(1){
+        if (state == 0 && inp[head] == '0'){
+            inp[head] = 'x';
+            state = 1; //move right
+            head++;
+            printf("input : %s head : %d, state : %d\n",inp,head, state);
+        }
+        else if (state == 1 && (inp[head] == '0' || inp[head]== 'y')){
+            //printf("Show\n");
+			if(inp[head]=='0'){
+				inp[head]='0';
+			}
+			else if (inp[head] == 'y'){
+				inp[head] = 'y' ;
+			}
+            state = 1;
+            //printf("\n\nState : %d",state);
+            if(inp[head] != '1'){
+                head++;
+            }
+            printf("\n\nHEAD : %d",head);
+
+        }
+        else if (state == 1 && inp[head] == '1'){
+            inp[head] = 'y';
+            printf("\n\nlast Input  : %s",inp);
+            state = 2;
+            head--;
+            printf("\n\n Current HEAD : %d\n",head);
+        }
+
+        else if (state == 2 && (inp[head]=='0' || inp[head]=='y')){
+			if(inp[head] =='0'){
+				inp[head]= '0';
+			}
+			else if(inp[head] == 'y'){
+				inp[head]='y';
+			}
+            while(inp[head] != 'x'){
+                head--;//move left
+                state = 2;
+                printf("\n\n State 2 HEAD : %d\n",head);
+            }
+        }
+
+        else if(state == 2 && inp[head]=='x'){
+        		inp[head] = 'x';
+         	 head++;
+            state = 0;
+            printf("\n\n State 2 secod part HEAD Update : %d, inp %s state 2 end\n",head, inp);
+        }
+
+		else if(state ==0 && inp[head] == 'y'){
+			state = 3;// move right
+			head++;
+		}
+
+
+		else if(state ==3&&inp[head]=='y'){
+			while(inp[head]   == 'y'){
+				head++;
+			}
+			if(inp[head]=='\0'){
+				state = 4;
+				//break;
+			}
+		}
+
+        else{
+        		printf("final result is %s :  cmhead %d status state %d\n",inp,head,state);
+            break;
+        }
+    }// while loooop end
+	if (state == 4){
+		printf("accept\n");
+	}
+	else{
+		printf("Reject\n");
+	}
+return 0;
+}
+
